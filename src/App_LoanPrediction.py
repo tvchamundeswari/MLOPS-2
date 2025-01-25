dc970a31842ac667d850ca6e625486117154a581
T V CHAMUNDESWARI DEVI <63962259+tvchamundeswari@users.noreply.github.com>
2025-01-26 00:06:52 +0530

    Update App_LoanPrediction.py

--- src/App_LoanPrediction.py
+++ src/App_LoanPrediction.py
@@ -1,9 +1,10 @@
-#om 
+# Import necessary libraries
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import Inference
 
 if __name__ == "__main__":
-    prediction = Inference.predict_loan_servicability("0","0","1000","50000")
-    print("Prdiction on Load Servicability: ", prediction)
+    # Perform loan serviceability prediction
+    prediction = Inference.predict_loan_servicability("0", "0", "1000", "50000")
+    print("Prediction on Loan Servicability:", prediction)
