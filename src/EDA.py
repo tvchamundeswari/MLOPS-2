45c31fb21cc28dbf592d45d5ca2c925d1935fa3f
Chamundeswari <komalitangirala2000@gmail.com>
2025-01-25 23:24:43 +0530

    edits

--- src/EDA.py
+++ src/EDA.py
@@ -1,13 +1,14 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sb
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.preprocessing import LabelEncoder
 
 import warnings
+
 warnings.filterwarnings('ignore')
+
+
 def performEDA(df):
-        
     df = pd.read_csv('src/loan_data.csv')
     df.head()
     df.shape
@@ -15,28 +16,26 @@ def performEDA(df):
     df.describe()
 
     temp = df['Loan_Status'].value_counts()
-    plt.pie(temp.values,
-            labels=temp.index,
-            autopct='%1.1f%%')
+    plt.pie(temp.values, labels=temp.index, autopct='%1.1f%%')
     plt.show()
 
     plt.subplots(figsize=(15, 5))
     for i, col in enumerate(['Gender', 'Married']):
-        plt.subplot(1, 2, i+1)
+        plt.subplot(1, 2, i + 1)
         sb.countplot(data=df, x=col, hue='Loan_Status')
     plt.tight_layout()
     plt.show()
 
     plt.subplots(figsize=(15, 5))
     for i, col in enumerate(['ApplicantIncome', 'LoanAmount']):
-        plt.subplot(1, 2, i+1)
+        plt.subplot(1, 2, i + 1)
         sb.distplot(df[col])
     plt.tight_layout()
     plt.show()
 
     plt.subplots(figsize=(15, 5))
     for i, col in enumerate(['ApplicantIncome', 'LoanAmount']):
-        plt.subplot(1, 2, i+1)
+        plt.subplot(1, 2, i + 1)
         sb.boxplot(df[col])
     plt.tight_layout()
     plt.show()
@@ -44,9 +43,8 @@ def performEDA(df):
     df = df[df['ApplicantIncome'] < 25000]
     df = df[df['LoanAmount'] < 400000]
 
-    df.groupby('Gender').mean(numeric_only=True)['LoanAmount']
-
-    df.groupby(['Married', 'Gender']).mean(numeric_only=True)['LoanAmount']
+    print(df.groupby('Gender').mean(numeric_only=True)['LoanAmount'])
+    print(df.groupby(['Married', 'Gender']).mean(numeric_only=True)['LoanAmount'])
 
     # Function to apply label encoding
     def encode_labels(data):
--- src/Train.py
+++ src/Train.py
@@ -1,84 +1,62 @@
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import StandardScaler
 from sklearn import metrics
 from sklearn.svm import SVC
 from imblearn.over_sampling import RandomOverSampler
 import joblib
-
 import EDA as EDA
 import Preprocessor as Preprocessor
 
-#Loading the dataset and prprocessin
+# Loading the dataset and preprocessing
 df = Preprocessor.load_data()
 df = EDA.performEDA(df)
-df.drop('Loan_ID',axis=1,inplace=True)
+df.drop('Loan_ID', axis=1, inplace=True)
 
 # Start Model Training
 features = df.drop('Loan_Status', axis=1)
 target = df['Loan_Status'].values
 
-X_train, X_val,	Y_train, Y_val = train_test_split(features, target,
-									test_size=0.2,
-									random_state=10)
+X_train, X_val, Y_train, Y_val = train_test_split(
+    features, target, test_size=0.2, random_state=10
+)
 
-# As the data was highly imbalanced we will balance
-# it by adding repetitive rows of minority class.
-ros = RandomOverSampler(sampling_strategy='minority',
-						random_state=0)
+# Balancing the data by oversampling
+ros = RandomOverSampler(sampling_strategy='minority', random_state=0)
 X, Y = ros.fit_resample(X_train, Y_train)
 
-#X_train.shape, X.shape
-print("Dataset : training data sample :", X_train.head())
+print("Dataset: training data sample:", X_train.head())
 
-#Normalizing the features for stable and fast training.
+# Normalizing the features
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
 X_val = scaler.transform(X_val)
 
-#Apply GridSearchCV for best model parameters:
-from sklearn.model_selection import GridSearchCV
-
-# Assigning the parameters and its values which need to be tuned.
-svc_parameters = {'kernel': ['linear', 'rbf'], 'C':[4,5,6,7,10,15]}
- 
-# Fitting the SVM model
+# Apply GridSearchCV for best model parameters
+svc_parameters = {'kernel': ['linear', 'rbf'], 'C': [4, 5, 6, 7, 10, 15]}
 modelsvc = SVC()
- 
-# Performing the GridSearchCV
-clf = GridSearchCV(modelsvc, svc_parameters, cv = 10, scoring='accuracy')
-print("Starting the gridsearchcv")
-clf.fit(X, Y) #, )
+clf = GridSearchCV(modelsvc, svc_parameters, cv=10, scoring='accuracy')
+
+print("Starting the grid search CV")
+clf.fit(X, Y)
 
 print(clf.best_params_)
-
 print(clf.best_score_)
 
 final_model = SVC(kernel=clf.best_params_['kernel'], C=clf.best_params_['C'])
 final_model.fit(X, Y)
 
-print('Training Accuracy : ', metrics.roc_auc_score(Y, final_model.predict(X)))
-print('Validation Accuracy : ', metrics.roc_auc_score(Y_val, final_model.predict(X_val)))
-print()
+print('Training Accuracy:', metrics.roc_auc_score(Y, final_model.predict(X)))
+print('Validation Accuracy:', metrics.roc_auc_score(Y_val, final_model.predict(X_val)))
 
 print("Save the model")
-joblib.dump(final_model,'model.joblib')
+joblib.dump(final_model, 'model.joblib')
 
-## tEST
-print("Prdiction : ", final_model.predict([["0","0","1000","50000"]]))
+print("Prediction:", final_model.predict([[0, 0, 1000, 50000]]))
 
 score = final_model.score(X_val, Y_val)
-print('Accuracy: ', score)
+print('Accuracy:', score)
 
-from sklearn.svm import SVC
-from sklearn.metrics import confusion_matrix
-#training_roc_auc = roc_auc_score(Y, model.predict(X))
-#validation_roc_auc = roc_auc_score(Y_val, model.predict(X_val))
-#print('Training ROC AUC Score:', training_roc_auc)
-#print('Validation ROC AUC Score:', validation_roc_auc)
-#print()
+from sklearn.metrics import confusion_matrix, classification_report
+
 cm = confusion_matrix(Y_val, final_model.predict(X_val))
-
-from sklearn.metrics import classification_report
 print(classification_report(Y_val, final_model.predict(X_val)))
-
-#
