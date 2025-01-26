70d3e2810465a7d2e16a3c56b46bcb26ff4d3772
T V CHAMUNDESWARI DEVI <63962259+tvchamundeswari@users.noreply.github.com>
2025-01-26 12:41:05 +0530

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -6,12 +6,12 @@ class TestModel(unittest.TestCase):
 
     def test_model_presence(self):
         model = joblib.load('model.joblib')
-        self.assertFalse(model is None)
+        self.assertIsNotNone(model)
 
     def test_model_prediction(self):
         model = joblib.load('model.joblib')
-        prediction = model.predict([["0", "0", "1000", "50000"]])
-        self.assertTrue(prediction ,[1])
+        prediction = model.predict([[0, 0, 1000, 50000]])
+        self.assertIn(prediction[0], [0, 1])
 
 
 if __name__ == '__main__':
